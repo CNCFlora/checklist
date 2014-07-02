835446b13cd92e6e102adf64e5a8ccae92372eae
bgiminiani <giminiani@gmail.com>
2014-07-02 14:06:55 -0300

    Until making tests.

--- src/app.rb
+++ src/app.rb
@@ -6,7 +6,7 @@ require 'sinatra/config_file'
 require 'sinatra/mustache'
 require "sinatra/reloader" if development?
 
-require 'cncflora_commons'
+require_relative 'cncflora_commons'
 require 'couchdb_basic'
 
 =begin
@@ -14,6 +14,7 @@ if development?
         also_reload "routes/*"
 end
 =end
+
 
 config_file ENV['config'] || '../config.yml'
 use Rack::Session::Pool
@@ -25,14 +26,20 @@ config = etcd2settings(ENV["ETCD"] || settings.etcd)
 config[:connect] = "#{config[:connect_url]}"
 config[:datahub] = "#{config[:datahub_url]}"
 config[:couchdb] = "#{config[:datahub_url]}/#{settings.db}"
-config[:elasticsearch] = "#{config[:datahub_url]}/#{settings.db}"
+#config[:elasticsearch] = "#{config[:datahub_url]}/#{settings.db}"
+config[:elasticsearch] = "#{config[:datahub_url]}"
 config[:strings] = JSON.parse(File.read("src/locales/#{settings.lang}.json", :encoding => "BINARY"))
 config[:services] = "#{config[:dwc_services_url]}/api/v1"
 config[:base] = settings.base
 set :elasticsearch, config[:elasticsearch]
 
-puts "elasticsearch = #{settings.elasticsearch}"
 puts "config = #{config}"
+checklists = "#{config[:datahub]}/checklists"
+taxonomy = "#{config[:datahub]}/taxonomy"
+
+
+#database = http_put("#{config[:datahub]}/test22222","")
+#puts "http_put = #{database}"
 =begin
 puts "datahub = #{config[:datahub]}"
 puts "couchdb = #{config[:couchdb]}"
@@ -53,16 +60,75 @@ end
 
 
 get "/" do
-    mustache :index, {}
+    databases = http_get("#{checklists}/_all_docs?include_docs=true")
+    databases = databases["rows"]
+    view :index, {:databases=>databases}
 end
 
-get "/new_checklist" do
-    mustache :new_checklist, {}
+get "/new" do
+    mustache :new, {}
 end
 
-get "/edit_checklist" do
-    mustache :edit_checklist, {}
+get "/delete/id/:_id/rev/:_rev" do
+    http_delete("#{checklists}/#{params[:_id]}?rev=#{params[:_rev]}")
+    databases = http_get("#{checklists}/_all_docs?include_docs=true")
+    databases = databases["rows"]
+    view :index, {:databases=>databases}
 end
+
+get "/edit/id/:_id/rev/:_rev" do
+    doc = {}
+    doc["_id"] = params["_id"]
+    doc["_rev"] = params["_rev"]
+    doc["metadata"] = {:type=>"checklist"}
+    families = search("taxonomy","metadata.type=\"taxonomy\" AND taxonRank:\"family\"")
+    families = families.sort_by {|f| f["family"]}
+    doc["families"] = families
+    view :edit, doc 
+end
+
+get "/edit/id/:_id/rev/:_rev/family/:family" do
+    id = params[:_id]
+    checklist = search("checklists","metadata.type=\"checklist\" AND _id:\"#{id}\"").first
+    doc = {}
+    doc["name"] = checklist["name"]
+    doc["description"] = checklist["description"]
+ 
+    puts "checklist = #{checklist}"
+
+    species = search("taxonomy","metadata.type=\"taxonomy\" AND taxonRank:\"species\" AND family:\"#{params[:family]}\"")    
+    puts "species = #{species}"
+    view :edit, {}
+end
+
+
+post "/edit" do
+    doc = {}
+    doc["name"] = params["name"]
+    doc["description"] = params["description"]
+    doc["metadata"] = {:type=>"checklist"} 
+    result = http_post("#{checklists}",doc)
+    puts "result = #{result}"
+    doc["_id"] = result["id"]
+    doc["_rev"] = result["rev"]
+    families = search("taxonomy","metadata.type=\"taxonomy\" AND taxonRank:\"family\"")
+    families = families.sort_by {|f| f["family"]}
+    doc["families"] = families
+    view :edit, doc
+end
+
+=begin
+post "/edit/id/:_id/rev/:_rev" do
+    doc = {}
+    doc["_id"] = params["_id"]
+    doc["_rev"] = params["_rev"]
+    doc["metadata"] = {:type=>"checklist"} 
+    http_put("#{checklists}",doc)
+    families = search("#{taxonomy}","metadata.type=\"taxonomy\" AND taxonRank=\"family\"")
+    doc["families"] = families
+    view :edit, doc
+end
+=end
 
 get "/species" do
     mustache :species, {}
---
+++ src/cncflora_commons.rb
@@ -0,0 +1,104 @@
+require 'uri'
+require 'json'
+require 'net/http'
+
+def http_get(uri)
+    JSON.parse(Net::HTTP.get(URI(uri)))
+end
+
+def http_post(uri,doc)
+    uri = URI.parse(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    if doc.class == Hash
+        header = {'Content-Type'=> 'application/json'}
+    elsif doc.class == String
+        header = {'Content-Type'=> 'application/x-www-form-urlencoded'}
+    end
+
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+
+    if doc.class == Hash
+        request.body = doc.to_json
+    elsif doc.class == String
+        request.body = doc
+    end
+
+    response = http.request(request)
+    JSON.parse(response.body)
+end
+
+def http_put(uri,doc) 
+    uri = URI.parse(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    if doc.class == Hash
+        header = {'Content-Type'=> 'application/json'}
+    elsif doc.class == String
+        header = {'Content-Type'=> 'application/x-www-form-urlencoded'}
+    end
+
+    request = Net::HTTP::Put.new(uri.request_uri, header)
+
+    if doc.class == Hash
+        request.body = doc.to_json
+    elsif doc.class == String
+        request.body = doc
+    end
+
+    response = http.request(request)
+
+    JSON.parse(response.body)
+end
+
+def http_delete(uri)
+    uri = URI.parse(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Delete.new(uri.request_uri)
+    response = http.request(request)
+    JSON.parse(response.body)
+end
+
+def search(index,query)
+    query="scientificName:'Aphelandra longiflora'" unless query != nil && query.length > 0
+    result = []
+    r = http_get("#{settings.elasticsearch}/#{index}/_search?size=999&q=#{URI.encode(query)}")
+    puts "elastic = #{settings.elasticsearch}"
+    puts "index = #{index}"
+    puts "r = #{settings.elasticsearch}/#{index}/_search?size=999&q=#{URI.encode(query)}"
+    r['hits']['hits'].each{|hit|
+        result.push(hit["_source"])
+    }
+    result
+end
+
+def flatten(obj)
+    flat = {}
+    if obj["dir"] && obj["nodes"] then
+        obj["nodes"].each { |n|
+            flat = flat.merge(flatten(n))
+        }
+    else
+        key = obj["key"].gsub("/","_").gsub("-","_")
+        flat[key[1..key.length].to_sym]=obj["value"]
+    end
+        flat
+end
+
+def etcd2config(server)
+    config = flatten( http_get("#{server}/v2/keys/?recursive=true")["node"] )
+    config.keys.each {|k|
+        if k.to_s.end_with? "_url" then
+            config[k.to_s.gsub("_url","").to_sym] = config[k]
+        end
+    }
+    config
+end
+
+def etcd2settings(server)
+    config = etcd2config(server)
+    config.keys.each { |key| set key, config[key] }
+    set :config, config
+    config
+end
+
