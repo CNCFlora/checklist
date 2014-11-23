fc887c566e93c9a32dd6f41fa4b4887edce89429
bgiminiani <giminiani@gmail.com>
2014-11-23 20:17:36 -0200

    More tests.

--- src/models/entity.rb
+++ src/models/entity.rb
@@ -1,9 +1,15 @@
+require 'rubygems'
 require 'json'
 require 'json-schema'
 
 
 class Entity
  #attr_reader :datetime, :duration, :class, :price, :level
+
+=begin
+    def initialize(schema,data)
+    end
+=end
 
     def init(data, recursion)
         data.each do |name, value|
@@ -21,7 +27,6 @@ my_hash = JSON.parse('{"hello": "goodbye"}')
 puts my_hash["hello"]
 
 schema = '{
-    "person": {
         "type": "object",
         "required":["last_name"],
         "additionalProperties": false,
@@ -49,7 +54,6 @@ schema = '{
                  }
             }
         }
-    }
 }'
 
 person = '{ "name":"Bruno", "test":"", "data_birth":"20-12-1978", "address":{"street":"Sao Domingos", "number":85}}'
@@ -63,7 +67,34 @@ puts "JSON.load(schema) = #{person}"
 #person = {"name" => "Bruno", "test"=>nil, "data_birth" => "20-12-1978", "address" => {"street" => "Sao Domingos", "number" => 85}}
 #puts "schema = #{schema}"
 #puts "person = #{person}"
-puts "validate = #{JSON::Validator.validate!(schema,person)}"
+begin
+    JSON::Validator.validate!(schema, person)
+rescue JSON::Schema::ValidationError
+  puts $!.message
+end
+#errors = JSON::Validator.validate!(schema,person)
+
+
+schema = {
+  "type" => "object",
+    "required" => ["a"],
+      "properties" => {
+        "a" => {"type" => "integer"}
+          }
+}
+
+data = {
+  "b" => 5
+}
+
+#puts JSON::Validator.validate(schema, data)
+
+hash = [1,"a"].inject({}) do |hash, item|
+#[1,"a",Object.new,:hi].inject({}) do |hash, item|
+  hash[item.to_s] = item
+  hash
+end
+puts "hash = #{hash}"
 =begin
 =end
 # To do
