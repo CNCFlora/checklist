ada2562de7b33ea3f96d86f6cfbc1c7301dbb0cc
Ícaro de Carvalho <caro de Carvalho>
2017-07-17 22:50:52 -0300

    Adicionando icone para especies endemicas do brasil.

--- src/app.rb
+++ src/app.rb
@@ -106,11 +106,13 @@ get "/:db/edit/family/:family" do
         specie["synonyms"] = search(params[:db],"taxon", "taxonomicStatus:\"synonym\" AND acceptedNameUsage:\"#{specie["scientificNameWithoutAuthorship"]}*\"").sort_by { |element| element["scientificNameWithoutAuthorship"]}
 
         currentTaxon = http_get("#{settings.floradata}/api/v1/specie?scientificName=#{specie["scientificNameWithoutAuthorship"]}")["result"]
-        ###
-        endemic = http_get("http://servicos.jbrj.gov.br/flora/endemism/#{specie['scientificNameWithoutAuthorship']}")["result"]
 
-        if(!endemic["endemism"].nil? && endemic["endemism"] == "Endemic") then
-          specie["endemic"]=true;
+        endemic = http_get(URI.encode("http://servicos.jbrj.gov.br/flora/endemism/#{specie['scientificNameWithoutAuthorship']}"))["result"]
+
+        if !endemic.nil? then
+          if(!endemic[0]["endemism"].nil? && endemic[0]["endemism"] == "Endemic") then
+            specie["endemic"]=true;
+          end
         end
 
         if currentTaxon.nil? then
